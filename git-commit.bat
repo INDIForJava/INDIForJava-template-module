ced4c8ce3328f0bf57952ef390f6d09c7dd74841
Marco Cipriani <marco.cipriani.01@gmail.com>
2021-09-06 12:45:09 +0200

    Update

--- git-commit.bat
+++ git-commit.bat
@@ -1,13 +1,8 @@
 @echo off
 git add -A
 set /p msg="Git message: "
-echo "Git status:"
-git status
 git fetch
 git commit -m %msg%
-set /p tag="Git tag: "
-git tag %tag%
 echo "Git push..."
 git push
-git push --tag
 echo "Done."
